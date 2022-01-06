4a0bd888695534380ff6396a85764b0b4432ec4e
Antonina Chichikalova <tonia.chichikalova@gmail.com>
2022-01-06 15:35:20 -0500

    Initializy react app, refuctore the code and ready to start

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBRCSOZlrjL-hzqLWw_D6Fy2Wht_4oPtH4",
+  authDomain: "netflix-clone-4e8a2.firebaseapp.com",
+  projectId: "netflix-clone-4e8a2",
+  storageBucket: "netflix-clone-4e8a2.appspot.com",
+  messagingSenderId: "588780350521",
+  appId: "1:588780350521:web:b5ad4cd5ae02fcd02631e8"
+};
