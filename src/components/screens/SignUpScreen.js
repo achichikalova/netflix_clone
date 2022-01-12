b757acbec40a46679d6f63cee6826ced1e12c90a
Antonina Chichikalova <tonia.chichikalova@gmail.com>
2022-01-12 15:14:52 -0500

    Implement authentication with firebase

--- src/components/screens/SignUpScreen.js
+++ src/components/screens/SignUpScreen.js
@@ -1,22 +1,46 @@
+import { useRef } from 'react';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 import './SignUpScreen.css';
+import { auth } from '../../firebase';
 
 const SignUpScreen = () => {
 
-  const register = (e) => {
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+
+  const register = async (e) => {
     e.preventDefault();
+
+    try {
+      const user = await createUserWithEmailAndPassword(auth, emailRef.current.value, passwordRef.current.value);
+      console.log(user)
+    } catch (error) {
+      alert(error.message)
+    }
   }
 
-  const signIn = (e) => {
+  const login = async (e) => {
+    e.preventDefault();
+
+    try {
+      const user = await signInWithEmailAndPassword(auth, emailRef.current.value, passwordRef.current.value);
+      console.log(user)
+    } catch (error) {
+      alert(error.message)
+    }
+  }
+
+  const logout = async (e) => {
     e.preventDefault();
   }
 
   return (
     <div className='signUp-screen'>
-      <form action="">
+      <form>
         <h1>Sign In</h1>
-        <input type="email" name="email" placeholder='Email' />
-        <input type="password" name="password" placeholder="Password" />
-        <button type='submit' onClick={signIn}>Sign In</button>
+        <input ref={emailRef} type="email" name="email" placeholder='Email' />
+        <input ref={passwordRef} type="password" name="password" placeholder="Password" />
+        <button type='submit' onClick={login}>Sign In</button>
       </form>
       <div className='link'>
         <span>New to Netflix? <a href="/" onClick={register}>Sign up now.</a></span>
