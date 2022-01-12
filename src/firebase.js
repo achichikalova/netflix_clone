e2d6ad14ff057bb195073a2e14fcd7d0517957a4
Antonina Chichikalova <tonia.chichikalova@gmail.com>
2022-01-12 12:14:12 -0500

    Initialaize firebase authentication

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+
 const firebaseConfig = {
   apiKey: "AIzaSyBRCSOZlrjL-hzqLWw_D6Fy2Wht_4oPtH4",
   authDomain: "netflix-clone-4e8a2.firebaseapp.com",
@@ -6,3 +8,10 @@ const firebaseConfig = {
   messagingSenderId: "588780350521",
   appId: "1:588780350521:web:b5ad4cd5ae02fcd02631e8"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { auth };
+export default db;
