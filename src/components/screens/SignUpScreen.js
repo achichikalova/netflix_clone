503dd2d4687242852d0e943e9204228c78278a52
Antonina Chichikalova <tonia.chichikalova@gmail.com>
2022-01-12 12:01:48 -0500

    Start working on authentication

--- src/components/screens/SignUpScreen.js
+++ src/components/screens/SignUpScreen.js
@@ -1,14 +1,26 @@
 import './SignUpScreen.css';
 
 const SignUpScreen = () => {
+
+  const register = (e) => {
+    e.preventDefault();
+  }
+
+  const signIn = (e) => {
+    e.preventDefault();
+  }
+
   return (
     <div className='signUp-screen'>
       <form action="">
         <h1>Sign In</h1>
         <input type="email" name="email" placeholder='Email' />
         <input type="password" name="password" placeholder="Password" />
-        <button type='submit'>Sign In</button>
+        <button type='submit' onClick={signIn}>Sign In</button>
       </form>
+      <div className='link'>
+        <span>New to Netflix? <a href="/" onClick={register}>Sign up now.</a></span>
+      </div>
     </div>
   )
 }
