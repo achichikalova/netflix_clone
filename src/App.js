9bc606bb6cccb0a6f3fbb5fe46025fb6f4ee357e
Antonina Chichikalova <tonia.chichikalova@gmail.com>
2022-01-12 15:18:21 -0500

    Start working on passing user to all components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import HomeScreen from './components/screens/HomeScreen';
 import LoginScreen from './components/screens/LoginScreen';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Nav from './components/Nav';
+import { auth } from './firebase';
 
 function App() {
 
   const [user, setUser] = useState('');
-  
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(userAuth => {
+      if (userAuth) {
+        // Logged in
+        setUser(userAuth)
+      } else {
+        // Logged out
+      }
+    })
+
+    return unsubscribe;
+  }, []);
+
   return (
     <Router>
       <Nav />
