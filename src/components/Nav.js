94e8e7b1125da49616b73961acd18f04fe187b15
Antonina Chichikalova <tonia.chichikalova@gmail.com>
2022-01-12 22:17:43 -0500

    Add redirection functionality to profile and home pages

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,18 +1,21 @@
+import { useNavigate } from 'react-router-dom';
 import './Nav.css';
 
-const Nav = ({user}) => {
+const Nav = ({ user }) => {
+
+  const navigate = useNavigate();
   
   return (
     <div className='nav'>
       <div className="nav-content">
-        <img className='nav-logo' src="./images/logo.png" alt="" />
+        <img onClick={() => {navigate('/')}} className='nav-logo' src="./images/logo.png" alt="" />
         {!user ? (
         <div className="nav-btn">
           <button className='btn'>English</button>
           <button className='btn btn--red'>Sign In</button>
         </div>
         ) : 
-          <img className='user-icon' src="./images/user.png" alt="" />
+          <img onClick={() => {navigate('/profile')}} className='user-avatar' src="./images/user.png" alt="" />
         }
       </div>
     </div>
