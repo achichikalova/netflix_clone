a4b490e99092f3454f7a659772cf1880f75a25eb
Antonina Chichikalova <tonia.chichikalova@gmail.com>
2022-01-12 22:14:59 -0500

    Start working on Profile page, finish main layout

---
+++ src/components/screens/ProfileScreen.js
@@ -0,0 +1,30 @@
+import { useSelector } from 'react-redux';
+import { selectUser } from '../../features/userSlice';
+import { auth } from '../../firebase';
+import './ProfileScreen.css';
+
+const ProfileScreen = () => {
+
+  const user = useSelector(selectUser);
+
+  return (
+    <div className='profileScreen'>
+      <div className="profileScreen-content">
+        <h1>Edit Profile</h1>
+        <div className="profileScreen-info">
+          <img src="./images/user.png" alt="user-avatar" />
+          <div className="profileScreen-details">
+            <h2>{user.email}</h2>
+            <div className="profileScreen-plans">
+              <h3>Plans</h3>
+              <p></p>
+              <button onClick={() => auth.signOut()} className='profileScreen-signOut'>Sign Out</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ProfileScreen;
