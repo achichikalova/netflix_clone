0a2197a294f0230ac8548bdf5c0d65da26238c64
Antonina Chichikalova <tonia.chichikalova@gmail.com>
2022-01-17 16:52:47 -0500

    Fix link address

--- src/components/Row.js
+++ src/components/Row.js
@@ -5,7 +5,7 @@ import "./Row.css";
 const Row = ({ title, fetchUrl, isLargeRow=false }) => {
   const [movies, setMovies] = useState([]);
 
-  const base_url = "https://image.tmdb.org/t/p/original/";
+  const base_url = "https://image.tmdb.org/t/p/original";
 
   useEffect(() => {
     async function fetchData() {
