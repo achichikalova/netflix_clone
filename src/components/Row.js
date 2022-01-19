17081fab135b2099137795320e0dcdb437e40e59
Antonina Chichikalova <tonia.chichikalova@gmail.com>
2022-01-18 19:13:50 -0500

    Implement drag and swipe animation to movie cards

--- src/components/Row.js
+++ src/components/Row.js
@@ -7,6 +7,7 @@ const Row = ({ title, fetchUrl, isLargeRow=false }) => {
 
   const base_url = "https://image.tmdb.org/t/p/original";
 
+  // Get data from API and set it to movies
   useEffect(() => {
     async function fetchData() {
       const request = await axios.get(fetchUrl);
@@ -17,6 +18,7 @@ const Row = ({ title, fetchUrl, isLargeRow=false }) => {
     fetchData();
   }, [fetchUrl]);
 
+  // Get movie element
   const moviePoster = movies.map(movie => {
     return <img
       className={`row-poster ${isLargeRow && "row-poster--large"}`}
@@ -25,6 +27,40 @@ const Row = ({ title, fetchUrl, isLargeRow=false }) => {
       isLargeRow ? movie.poster_path : movie.backdrop_path}`}
       alt={movie.name} />
   });
+
+  // Drag and swipe
+  useEffect(() => {
+    const sliders = document.querySelectorAll('.row-posters');
+    
+    for (const slider of sliders) {
+      let isDown = false;
+      let startX;
+      let scrollLeft;
+      slider.addEventListener('mousedown', async (e) => {
+        isDown = true;
+        slider.classList.add('active');
+        startX = e.pageX - slider.offsetLeft;
+        scrollLeft = slider.scrollLeft;
+      });
+      slider.addEventListener('mouseleave', async () => {
+        isDown = false;
+        slider.classList.remove('active');
+      });
+      slider.addEventListener('mouseup', async () => {
+        isDown = false;
+        slider.classList.remove('active');
+      });
+      slider.addEventListener('mousemove', async (e) => {
+        if(!isDown) return;
+        e.preventDefault();
+        const x = e.pageX - slider.offsetLeft;
+        const walk = (x - startX) * 3; 
+        //scroll-fast
+        slider.scrollLeft = scrollLeft - walk;
+      });
+    }
+
+  }, []);
 
   return (
     <div className="row">
