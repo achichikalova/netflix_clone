30a875dfa3e61c059541513676f44c0c614c6238
Antonina Chichikalova <tonia.chichikalova@gmail.com>
2022-01-26 18:56:56 -0500

    WIP: working on implementing modal screen

--- src/components/screens/ModalScreen.js
+++ src/components/screens/ModalScreen.js
@@ -2,7 +2,18 @@ import './ModalScreen.css';
 
 const ModalScreen = () => {
   return (
-    <div>
+    <div className='modalScreen-container'>
+      <div className="modalScreen-body">
+        <button>X</button>
+        <div className="modalScreen-title">
+          <h1>Movie title</h1>
+        </div>
+        <div className="modalScreen-info">
+          <p>Description</p> 
+          <div className="modalScreen-rating">Rating</div>
+        </div>
+        <button>Play</button>
+      </div>
       
     </div>
   )
